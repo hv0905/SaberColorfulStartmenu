da2c4698474053d8c51f70c8f9db013ffadea0e5
Saber <hv0905@outlook.com>
2018-07-05 15:08:04 +0800

    publish V1.2

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace SaberColorfulStartmenu.Properties {
         }
         
         /// <summary>
-        ///   查找类似 Saber&apos;s Colorful Startmenu Version:1.1 (release 1.1)
+        ///   查找类似 Saber&apos;s Colorful Startmenu Version:1.2 (release 1.2)
         ///
         ///作者：HV0905
         ///
